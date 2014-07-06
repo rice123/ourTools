497b195ff28e9e9e88738bf6ae17d50d23bf6815
xiaozhujun <xiao520@163.com>
2014-07-06 09:47:28 +0800

    计算二进制数中1的数量，时间复杂度为1的个数。

---
+++ chenqw/src/OperateOP/CountOneInBinary.java
@@ -0,0 +1,20 @@
+package OperateOP;
+
+import java.util.Scanner;
+
+public class CountOneInBinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		int count=0;
+		while(num!=0)
+		{
+			count++;
+			num=(num-1)&num;
+		}
+		System.out.print(count);
+	}
+
+}
